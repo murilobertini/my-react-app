57ab856f2b272da8b7c89e27f37bd7964046a5b1
MURILO BERTINI <murilo@getting.co>
2018-07-31 00:31:28 -0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and Swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketch app, swift app course, arkit 2, after effects, create sketch plugin'
+
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'us9tjwuqahcx',
+        accessToken: '84b54144c30bcbc5d92ff5e14a49fe6f83737f2f51c41b21a7bd69f2c4f77d6a'
+      }
+    }
+  ],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,30 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+  <Helmet
+    title={data.site.siteMetadata.title}
+    meta={[
+    { name: 'description', content: data.site.siteMetadata.description },
+    { name: 'keywords', content: data.site.siteMetadata.keywords },
+    ]}
+  />
+    <Header />
+    {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React. <a href="mailto:support@designcode.io">Email us</a> to ask anything. © 2018
+    </Footer>
   </div>
 )
 
@@ -36,10 +30,21 @@ export default Layout
 
 export const query = graphql`
   query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
+	site {
+	  siteMetadata {
+		title
+		description
+		keywords
+	  }
+	}
+	allContentfulLink(sort: { fields: [createdAt], order: ASC }) {
+	  edges {
+		node {
+		  title
+		  url
+		  createdAt
+		}
+	  }
+	}
   }
 `
